9c248b1e5c7cb3005cebeb5f40fa4f3dffdfe485
baiyujia <zhlsuo@163.com>
2018-07-21 18:23:41 +0800

    1、增加房屋过期标志。 2、分析房屋时，能够按照楼盘数量top10，给出按照日期的统计变化驱使

--- house_analyze.py
+++ house_analyze.py
@@ -49,7 +49,7 @@ def price_going(data_df):
 
 
 
-'''
+
     for i in range(5,df_up.shape[1]+1,5):
         df_up.iloc[:,i-5:i].plot(kind  = 'line',figsize =(8,5),rot =90)
         matplotlib.pyplot.ylabel('总价(万元)',fontproperties=getChineseFont())
@@ -58,15 +58,16 @@ def price_going(data_df):
         matplotlib.pyplot.legend(prop=getChineseFont())
         matplotlib.pyplot.tight_layout()
         matplotlib.pyplot.savefig(PIC_PATH + '房价波动图' + str(int(i/5)) + '.png')
+
         matplotlib.pyplot.close()
-'''
+
 def price_stat(data_df):
     regex_str = None
     if (len(sys.argv) > 1):
         regex_str =  sys.argv[1]
 
     price_col = [col for col in data_df.columns if '总价' in col]
-    print('日期','房源数','单价均值','单价最大值','单价最小值','面积')
+    print('日期        ', '房源数   ', '均值     ', '最大值   ', '最小值', '中位数   ','面积   ')
     for col in price_col:
         df = data_df[np.isnan(data_df[col]) ^ True]
         if regex_str != None:
@@ -77,18 +78,19 @@ def price_stat(data_df):
             df = df[(df[col] < df[col].median() * 2 ) & (df[col] > df[col].median() / 2 )]
 
         valid_num = df.shape[0]
-        print(col,valid_num,
-              '%.2f' % (df[col].mean()/df['面积'].mean()),
-              '%.2f' % (df[col].max()/df['面积'].mean()),
-              '%.2f' % (df[col].min()/df['面积'].mean()),
-              '%.2f' % (df[col].median()/df['面积'].mean()),
-              '%.2f' % df['面积'].mean())
+        print(col.split('总价_')[1], '%8d' % valid_num,
+              '%8.3f' % (df[col].mean()),
+              '%8.0f' % (df[col].max() ),
+              '%8.0f' % (df[col].min() ),
+              '%8.0f' % (df[col].median()),
+              '%8.0f' % df['面积'].mean())
 
 def price_going_by_lp(data_df):
 
     price_col = [col for col in data_df.columns if '总价' in col]
-    print('日期', '房源数', '单价均值', '单价最大值', '单价最小值', '面积')
+    print('日期        ', '房源数   ', '均值     ', '最大值   ', '最小值', '中位数   ','面积   ','单价   ')
     lp_price_stat = []
+    last_price = 0
     for col in price_col:
         df = data_df[np.isnan(data_df[col]) ^ True]
 
@@ -97,14 +99,18 @@ def price_going_by_lp(data_df):
             df = df[(df[col] < df[col].median() * 2) & (df[col] > df[col].median() / 2)]
 
         valid_num = df.shape[0]
-        print(col, valid_num,
-              '%.3f' % (df[col].mean()),
-              '%.3f' % (df[col].max() ),
-              '%.3f' % (df[col].min() ),
-              '%.3f' % (df[col].median()),
-              '%.0f' % df['面积'].mean())
+        if (int(df[col].mean()) != last_price and valid_num > 10):
+            print(col.split('总价_')[1], '%8d' % valid_num,
+                  '%8.3f' % (df[col].mean()),
+                  '%8.0f' % (df[col].max() ),
+                  '%8.0f' % (df[col].min() ),
+                  '%8.0f' % (df[col].median()),
+                  '%8.0f' % df['面积'].mean(),
+                  '%8.0f' % df['单价'].mean())
 
-        lp_price_stat.append({'楼盘':df['楼盘名称'],'均价':(df[col].mean() / df['面积'].mean()),'日期':col.split('总价_')[1]})
+        last_price = int(df[col].mean())
+
+        lp_price_stat.append({'楼盘':df['楼盘名称'],'单价':(df['单价'].mean()),'日期':col.split('总价_')[1]})
         #print(df['单价'].mean())
 
     return lp_price_stat
@@ -112,10 +118,10 @@ if __name__ == '__main__':
     data_df = pd.read_csv(PIC_PATH + 'house' + C_DAY + '.csv',index_col='标题',low_memory=False)
 
     price_going(data_df)
-    #price_stat(data_df)
-    # gr = data_df.groupby(by='楼盘名称')
-    # top10_lp = gr.count().sort_values(by='面积',ascending = False).iloc[0:20]
-    # for lp_name  in top10_lp.index:
-    #     df = data_df[data_df['楼盘名称'] == lp_name]
-    #     print('统计走势:',lp_name,df.shape)
-    #     lp_price_stat = price_going_by_lp(df)
+    price_stat(data_df)
+    gr = data_df.groupby(by='楼盘名称')
+    top10_lp = gr.count().sort_values(by='面积',ascending = False).iloc[20:30]
+    for lp_name  in top10_lp.index:
+        df = data_df[data_df['楼盘名称'] == lp_name]
+        print('统计走势:',lp_name,df.shape)
+        lp_price_stat = price_going_by_lp(df)
